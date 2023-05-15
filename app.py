4ae4169dee5909ef2c273a127afbac2efdd431fd
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2023-05-15 14:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import importlib
 import PIL
 from IPython.display import display, HTML
+import subprocess
 
 
 st.title("Text to 3D Model with Text2Mesh")
@@ -19,7 +20,7 @@ st.header("Upload and Remesh Mesh")
 obj_file = st.file_uploader("Upload OBJ file", type=["obj"])
 if obj_file is not None:
         # Save the uploaded file
-        file_path = os.path.join("input_obj", obj_file.name)
+        file_path = os.path.join("./input_obj/", obj_file.name)
         with open(file_path, "wb") as f:
             f.write(obj_file.getvalue())
         
@@ -35,7 +36,8 @@ if st.button("Run Text2Mesh"):
     output_dir = "results/"
     st.write("Running Text2Mesh...")
     n_iter=750
-    os.system(f"python main.py --run branch --obj_path {file_path} --output_dir {output_dir} --prompt \"{prompt}\" --sigma 12.0 --clamp tanh --n_normaugs 4 --n_augs 1 --normmincrop 0.1 --normmaxcrop 0.4 --geoloss --colordepth 2 --normdepth 2 --frontview --frontview_std 4 --clipavg view --lr_decay 0.9 --clamp tanh --normclamp tanh --maxcrop 1.0 --save_render --seed 29 --n_iter {n_iter} --learning_rate 0.0005 --normal_learning_rate 0.0005 --standardize --no_pe --symmetry --background 1 1 1")
+    command = ["python", "main.py", "--run", "branch", "--obj_path", file_path, "--output_dir", output_dir, "--prompt", prompt, "--sigma", "12.0", "--clamp", "tanh", "--n_normaugs", "4", "--n_augs", "1", "--normmincrop", "0.1", "--normmaxcrop", "0.4", "--geoloss", "--colordepth", "2", "--normdepth", "2", "--frontview", "--frontview_std", "4", "--clipavg", "view", "--lr_decay", "0.9", "--clamp", "tanh", "--normclamp", "tanh", "--maxcrop", "1.0", "--save_render", "--seed", "29", "--n_iter", str(n_iter), "--learning_rate", "0.0005", "--normal_learning_rate", "0.0005", "--standardize", "--no_pe", "--symmetry", "--background", "1", "1", "1"]
+    subprocess.run(command, check=True)
     st.success("Text2Mesh complete")
 '''
 # Streamlit interface to display results
