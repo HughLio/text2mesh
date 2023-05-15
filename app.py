666e75fddcc39bdea3900ced170ed413105fb883
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2023-05-15 11:27:03 +0530

    up

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import trimesh
+import pymeshlab
+import argparse
+import cv2
+import os
+import matplotlib.pyplot as plt
+import importlib
+import PIL
+from base64 import b64encode
+from IPython.display import display, HTML
+from tqdm.auto import tqdm
+
+
+st.title("Text to 3D Model with Text2Mesh")
+
+# Function to remesh input mesh
+
+
+# Streamlit interface to upload and remesh mesh
+st.header("Upload and Remesh Mesh")
+obj_file = st.file_uploader("Upload OBJ file", type=["obj"])
+if obj_file is not None:
+    obj_path = "/app/sample_data/input.obj"
+    with open(obj_path, "wb") as f:
+        f.write(obj_file.getbuffer())
+    prompt = st.text_input("Enter prompt")
+    n_iter = st.number_input("Enter number of iterations", min_value=1, value=750, step=1)
+    remeshed_path = obj_path
+    st.success("Remeshing complete")
+
+# Streamlit interface to run text2mesh
+st.header("Run Text2Mesh")
+if os.path.isfile(remeshed_path):
+    output_dir = "./results"
+    st.write("Running Text2Mesh...")
+    os.system(f"python main.py --run branch --obj_path {remeshed_path} --output_dir {output_dir} --prompt \"{prompt}\" --sigma 12.0 --clamp tanh --n_normaugs 4 --n_augs 1 --normmincrop 0.1 --normmaxcrop 0.4 --geoloss --colordepth 2 --normdepth 2 --frontview --frontview_std 4 --clipavg view --lr_decay 0.9 --clamp tanh --normclamp tanh --maxcrop 1.0 --save_render --seed 29 --n_iter {n_iter} --learning_rate 0.0005 --normal_learning_rate 0.0005 --standardize --no_pe --symmetry --background 1 1 1")
+    st.success("Text2Mesh complete")
+
+# Streamlit interface to display results
+st.header("Display Results")
+if os.path.isdir(output_dir):
+    frames = []
+    for i in range(0, n_iter, 100):
+        img = cv2.imread(os.path.join(output_dir, f"iter_{i}.jpg"))
+        frames.append(img)
+        plt.figure(figsize=(20, 4))
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.axis("off")
+        plt.title(f"Iteration {i}")
+        st.pyplot()
+else:
+    st.warning("No output directory found.")
