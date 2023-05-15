2707d8c2ddd3b2734c65fab168c2ca077c062aba
Vishnu K V <104137499+vishnu6363@users.noreply.github.com>
2023-05-15 15:57:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,29 @@
 import streamlit as st
-import trimesh
-import argparse
-#import cv2
-import os
-import matplotlib.pyplot as plt
-import importlib
-import PIL
-from IPython.display import display, HTML
 import subprocess
+import tempfile
+import os
 
-
+# Set Streamlit app title
 st.title("Text to 3D Model with Text2Mesh")
 
-# Function to remesh input mesh
-
-
 # Streamlit interface to upload mesh
-st.header("Upload  Mesh")
+st.header("Upload Mesh")
 obj_file = st.file_uploader("Upload OBJ file", type=["obj"])
-'''
-if obj_file is not None:
-        # Save the uploaded file
-        file_path = os.path.join("input_obj", obj_file.name)
-        with open(file_path, "wb") as f:
-            f.write(obj_file.getvalue())
-        
-        st.success("File saved successfully!")'''
-       
+
 prompt = st.text_input("Enter prompt")
-#n_iter = st.number_input("Enter number of iterations", min_value=1, value=750, step=1)
-#remeshed_path = obj_path
-#st.success("Remeshing complete")
 
-# Streamlit interface to run text2mesh
+# Streamlit interface to run Text2Mesh
 if st.button("Run Text2Mesh"):
-    output_dir = "results"
-    n_iter = 750
-    st.write("Running Text2Mesh...")
-    command = f"python main.py --run branch --obj_path {obj_file} --output_dir {output_dir} --prompt \"{prompt}\" --sigma 12.0 --clamp tanh --n_normaugs 4 --n_augs 1 --normmincrop 0.1 --normmaxcrop 0.4 --geoloss --colordepth 2 --normdepth 2 --frontview --frontview_std 4 --clipavg view --lr_decay 0.9 --clamp tanh --normclamp tanh --maxcrop 1.0 --save_render --seed 29 --n_iter {n_iter} --learning_rate 0.0005 --normal_learning_rate 0.0005 --standardize --no_pe --symmetry --background 1 1 1"
-    subprocess.run(command, shell=True)
-    st.success("Text2Mesh complete")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        output_dir = temp_dir
+        n_iter = 750
+        st.write("Running Text2Mesh...")
+        command = f"python main.py --run branch --obj_path {obj_file.name} --output_dir {output_dir} --prompt \"{prompt}\" --sigma 12.0 --clamp tanh --n_normaugs 4 --n_augs 1 --normmincrop 0.1 --normmaxcrop 0.4 --geoloss --colordepth 2 --normdepth 2 --frontview --frontview_std 4 --clipavg view --lr_decay 0.9 --clamp tanh --normclamp tanh --maxcrop 1.0 --save_render --seed 29 --n_iter {n_iter} --learning_rate 0.0005 --normal_learning_rate 0.0005 --standardize --no_pe --symmetry --background 1 1 1"
+        subprocess.run(command, shell=True)
+        st.success("Text2Mesh complete")
 
-    # Retrieve the resulting image from the modified main.py script
-    final_img = main.run_text2mesh()  # Replace with the appropriate function call
-
-    # Display the resulting image
-    if final_img is not None:
-        st.image(final_img, caption="Resulting Image")
-    else:
-        st.warning("No image found.")
-'''
-# Streamlit interface to display results
-st.header("Display Results")
-if os.path.isdir(output_dir):
-    frames = []
-    for i in range(0, n_iter, 100):
-        img = cv2.imread(os.path.join(output_dir, f"iter_{i}.jpg"))
-        frames.append(img)
-        plt.figure(figsize=(20, 4))
-        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        plt.axis("off")
-        plt.title(f"Iteration {i}")
-        st.pyplot()
-else:
-    st.warning("No output directory found.")'''
+        # Display the resulting images
+        for i in range(0, n_iter, 100):
+            img_path = os.path.join(output_dir, f"iter_{i}.jpg")
+            if os.path.isfile(img_path):
+                st.image(img_path, caption=f"Iteration {i}")
